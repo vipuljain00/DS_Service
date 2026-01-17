0b74258166e630043782d5f8e39e158bf3ad7526
vipulj3 <vipul.jain@monkhub.com>
2026-01-17 13:54:02 +0530

    Add Kafka

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from flask import request, jsonify
 from app.service.messageService import MessageService   
+from kafka import KafkaProducer
+import json
 
 app = Flask(__name__)
+producer = KafkaProducer(
+    bootstrap_servers='3.7.198.0:9093',
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
 
 messageService = MessageService()
 
@@ -12,6 +18,7 @@ def handle_message():
     result = messageService.process_message(message)
     if result is None:
         return jsonify({"error": "Not a valid bank message"}), 400
+    producer.send('ExpenseDetails', value=result.model_dump())
     return jsonify(result.model_dump())
 
 @app.route('/', methods=['GET'])
--- src/app/dataModal/ExpenseDataModal.py
+++ src/app/dataModal/ExpenseDataModal.py
@@ -6,4 +6,5 @@ class ExpenseDataModal(BaseModel):
     amount: Optional[str] = Field(title="expenseAmount", description="Amount of the expense")
     merchant: Optional[str] = Field(title="merchantName", description="Merchant name to whom the expense is made")
     currency: Optional[str] = Field(title="currency", description="Currency of the transaction")
+    createdAt: Optional[str] = Field(title="createdAt", description="Date and time when the expense was created")
     
